ad01cfc06cc81b87a788ddf84f43ebbc16c6bf5f
WallacexMcCarthy <89401301+WallacexMcCarthy@users.noreply.github.com>
2022-09-19 09:37:54 -0600

    array list application

--- src/lists/controller/Controller.java
+++ src/lists/controller/Controller.java
@@ -8,10 +8,14 @@ public class Controller
 {
 
 	private Scanner keyboardInput;
+	private ArrayList<String> nameList;
+	private ArrayList<String> ageList;
 	
 	public Controller()
 	{
 		this.keyboardInput = new Scanner(System.in);
+		this.nameList = new ArrayList<String>();
+		this.ageList = new ArrayList<String>();
 	}
 	
 	
@@ -24,16 +28,31 @@ public class Controller
 	
 	private void listDemo()
 	{
-		List<String> wordList = new ArrayList<>();
-		System.out.println("What si the word?");
-		String firstWord = keyboardInput.nextLine();
-		keyboardInput.nextLine();
-		System.out.println("Type in another word ...");
-		String secondWord = keyboardInput.nextLine();
+//		System.out.println("What si the word?");
+//		String firstWord = keyboardInput.nextLine();
+//		keyboardInput.nextLine();
+//		System.out.println("Type in another word ...");
+//		String secondWord = keyboardInput.nextLine();
+		for (int index=0; index<3; index++)
+		{
+			System.out.println("Enter a sudent name: ");
+			String name = keyboardInput.nextLine();
+			keyboardInput.nextLine();
+			System.out.println("Enter " + name + "'s age: ");
+			String age = keyboardInput.nextLine();
+			keyboardInput.nextLine();
+			nameList.add(name);
+			ageList.add(age);
+			
+		}
 		
-		wordList.add(firstWord);
-		wordList.add(secondWord);
-		System.out.println(wordList);
+		System.out.println(nameList);
+		System.out.println(ageList);
+		for (int index = 0; index < nameList.size(); index++)
+		{
+			System.out.print("Name: " + nameList.get(index) + " | Age: " + ageList.get(index));
+			
+		}
 		
 	}
 
